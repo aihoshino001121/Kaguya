4eceebbdd2505028313c1674d7a71c64846cc4b3
aihoshino001121 <razzak001121@gmail.com>
2026-03-11 08:58:26 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
 config: {
   name: "owner",
-  author: "INDRA(UTSHO)",
+  author: "INDRA",
   role: 0,
   shortDescription: "Bot owner information",
   longDescription: "",
